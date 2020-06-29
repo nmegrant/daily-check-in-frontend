6222a578fbc7de5d424643e6d8ba4790a832d336
nmegrant <nme.grant@gmail.com>
2020-06-29 13:56:10 +0200

    Set up controlled form

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
 
 export default function FormPage() {
-  return <h1>How are you doing?</h1>;
+  const [today, setToday] = useState("");
+  const [tomorrow, setTomorrow] = useState("");
+  const [life, setLife] = useState("");
+
+  return (
+    <div>
+      <h1>How are you doing?</h1>
+
+      <form>
+        <FormControl>
+          <TextField
+            label="What did you do today?"
+            placeholder="What did you do today?"
+            multiline
+            variant="filled"
+            value={today}
+            onChange={(event) => setToday(event.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <TextField
+            label="What do you plan to do tomorrow?"
+            placeholder="What do you plan to do tomorrow?"
+            multiline
+            variant="filled"
+            value={tomorrow}
+            onChange={(event) => setTomorrow(event.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <TextField
+            label="How do you feel about your life right now?"
+            placeholder="How do you feel about your life right now?"
+            multiline
+            variant="filled"
+            value={life}
+            onChange={(event) => setLife(event.target.value)}
+          />
+        </FormControl>
+      </form>
+    </div>
+  );
 }
