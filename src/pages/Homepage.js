fcf1859d9bd6c2a88ca84e5263842c46c936603a
nmegrant <nme.grant@gmail.com>
2020-06-30 20:53:01 +0200

    Add styling to homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,8 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-// import Box from "@material-ui/core/Box";
-// import Link from "@material-ui/core/Link";
-// import List from "@material-ui/core/List";
-// import ListItem from "@material-ui/core/ListItem";
-// import Button from "@material-ui/core/Button";
-
 import {
   Typography,
-  TextField,
   Grid,
   Button,
   Link,
@@ -20,46 +13,73 @@ import {
 export default function Homepage() {
   const history = useHistory();
   return (
-    <Grid>
-      {" "}
-      <Typography variant="h3">Sentiment Assessment</Typography>
-      <h3>
-        Our site uses the{" "}
-        <Link href="https://www.npmjs.com/package/sentiment">sentiment</Link>{" "}
-        module to evaluate user stress levels. To improve accessibility we
-        include speech recognition.
-      </h3>
-      <p>
-        If you are feeling stresed, overwhelmed, or depressed please contact the
-        appropriate health authorities:{" "}
-        <Link href="https://www.government.nl/topics/mental-health-services/question-and-answer/help-for-mental-health-problems">
-          Where can I get help for mental health problems?
-        </Link>
-      </p>
-      <List>
-        <ListItem>For Alcoholics Anonymous dial: 020 625 6057</ListItem>
-        <ListItem>For Sexual Abuse hotline dial: 0900 899 8411</ListItem>
-        <ListItem>For SOS helpline dial: 0900 0767</ListItem>
-        <ListItem>For ChildLine dial: 0800 0432</ListItem>
-        <ListItem>
-          <Link href="https://access-nl.org/living-netherlands/emergency-numbers-and-other-useful-information/">
-            More information
+    <Grid
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+      spacing={3}
+    >
+      <Grid>
+        {" "}
+        <Typography variant="h3" style={{ marginTop: "30px" }}>
+          Sentiment Assessment
+        </Typography>
+        <h3>
+          Our site uses the{" "}
+          <Link href="https://www.npmjs.com/package/sentiment">sentiment</Link>{" "}
+          module to evaluate user stress levels. To improve accessibility we
+          include speech recognition.
+        </h3>
+        <p>
+          If you are feeling stresed, overwhelmed, or depressed please contact
+          the appropriate health authorities:{" "}
+          <Link href="https://www.government.nl/topics/mental-health-services/question-and-answer/help-for-mental-health-problems">
+            Where can I get help for mental health problems?
           </Link>
-        </ListItem>
-        <ListItem>
-          <Link href="https://www.thehagueinternationalcentre.nl/living-in-the-hague-region/health-care/mental-health">
-            {" "}
-            Even more information
-          </Link>
-        </ListItem>
-      </List>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => history.push("/form")}
+        </p>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={3}
       >
-        Go to Evaluation Form
-      </Button>
+        <List>
+          <ListItem>For Alcoholics Anonymous dial: 020 625 6057</ListItem>
+          <ListItem>For Sexual Abuse hotline dial: 0900 899 8411</ListItem>
+          <ListItem>For SOS helpline dial: 0900 0767</ListItem>
+          <ListItem>For ChildLine dial: 0800 0432</ListItem>
+          <ListItem>
+            <Link href="https://access-nl.org/living-netherlands/emergency-numbers-and-other-useful-information/">
+              More information
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://www.thehagueinternationalcentre.nl/living-in-the-hague-region/health-care/mental-health">
+              {" "}
+              Even more information
+            </Link>
+          </ListItem>
+        </List>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={3}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => history.push("/form")}
+          style={{ marginTop: "30px" }}
+        >
+          Start Assessment
+        </Button>
+      </Grid>
     </Grid>
   );
 }
