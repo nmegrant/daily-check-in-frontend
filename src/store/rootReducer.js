6fe295c522c460022c3a8ff347fcecb45b5e3662
nmegrant <nme.grant@gmail.com>
2020-06-30 12:09:59 +0200

    Update root reducer and add selector

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import user from "./user/reducer";
 import sentiment from "./sentiment/reducer";
+import text from "./text/reducer";
 
 export default combineReducers({
   user,
   sentiment,
+  text,
 });
