1dedd559a2bdf5f0381c7a55502540242c2d33c1
kevinjohannesson <kroon.kevin@gmail.com>
2020-07-01 17:54:19 +0200

    Created small bargraph for single score and larger areaGraph for multiple scores components

--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -5,6 +5,7 @@ import { selectSentiment, selectHistory } from "../store/sentiment/selectors";
 import { getSentimentHistoryThunkCreator } from "../store/sentiment/actions";
 
 import { Button } from "@material-ui/core";
+import {AreaGraph, BarGraph} from "../components/graph";
 
 export default function ResultsPage() {
   const dispatch = useDispatch();
@@ -16,20 +17,41 @@ export default function ResultsPage() {
 
   const history = useSelector(selectHistory());
   console.log(history);
+  const sentimentHistory = history.map((s, i)=>({...s, day: new Date(s.createdAt).getDate()}));
 
   const sentimentScore = useSelector(selectSentiment());
   return (
     <div>
       <h1>Results</h1>
       <h1>{sentimentScore}</h1>
+      <BarGraph
+        score={sentimentScore} 
+        min={-5} 
+        max={5} 
+        ticks={[-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]}
+      />
       <Button variant="contained" color="primary" onClick={getHistory}>
         See History
       </Button>
-      {history
-        ? history.map((i) => {
-            return <p key={i.id}>{i.score}</p>;
-          })
+      {history.length 
+        ? <AreaGraph 
+            data={sentimentHistory} 
+            min={-5}
+            max={5}
+            primary_key={'score'} 
+            x_dataKey={'day'}
+            x_label={'Date'}
+            y_label={'Score'}
+            secondary_key={'comparativeScore'} 
+            showSecondaryDataAsLine={true}
+            duotone={true}
+            y_ticks={[-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]}
+          />
         : null}
     </div>
   );
 }
+
+
+
+
