15751d50f45d84f15fd4f15acd71c5084c293ed1
nmegrant <nme.grant@gmail.com>
2020-07-01 14:53:54 +0200

    Create actions for messages

--- src/store/appstate/reducer.js
+++ src/store/appstate/reducer.js
@@ -1,13 +1,15 @@
 const initialState = {
   loading: false,
-  message: null,
+  messageInfo: null,
 };
 
 export default function appstateReducer(state = initialState, action) {
   switch (action.type) {
     case "SET_MESSAGE":
-      return { ...state, message: action.payload };
+      return { ...state, messageInfo: action.payload };
     case "CLEAR_MESSAGE":
-      return { ...state, message: null };
+      return { ...state, messageInfo: null };
+    default:
+      return state;
   }
 }
