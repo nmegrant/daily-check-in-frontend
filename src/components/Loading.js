e9baca352feb6328bd7f98504ba1ff20c6e4abd0
nmegrant <nme.grant@gmail.com>
2020-07-02 12:59:38 +0200

    Set up loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > * + *": {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+export default function Loading() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CircularProgress />
+    </div>
+  );
+}
